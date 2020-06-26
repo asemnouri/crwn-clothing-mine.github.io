3585c4d9c88ebf11e035d993ba45d02fb59bdffa
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-06-26 21:41:20 +0300

    lecture 117

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,20 +1,25 @@
 import CustomButton from '../custom-Button/custom-Button';
 import React from 'react'
 import './cart-dropdown.scss'
+import CartItem from '../cart-item/cart-item'
+import {connect} from 'react-redux'
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     
     <div className='cart-dropdown'> 
         <div className='cart-items'>
-           
+         { cartItems.map((cartItem)=>(<CartItem key={cartItem.id} item={cartItem}/>) )}
         </div>
         <CustomButton>GO TO CHECK OUT</CustomButton>
     </div>
 )
 
+const mapStateToProps=({cart:{cartItems}})=>({//make sure to destructure the cartItems
+    cartItems
+    })
 
 //you have access to dipatch in otherProps(when it is not passed as parameter),
 // so you dont have to write it again and can be used in pther props
-export default CartDropDown;
+export default connect(mapStateToProps)(CartDropDown);
 //the order of the HOC is very important because we need the info from connect to use withRouter after that 
 //or use the ninja way of connect two HOC
