567a352e44129cb419ee318bb6729f1cec36250f
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-06-26 17:35:03 +0300

    lecture 108

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './components/homePage/homePage'
-import {Route , Switch ,  BrowserRouter} from 'react-router-dom'
+import {Route , Switch ,  BrowserRouter,Redirect} from 'react-router-dom'
 import './App.css';
 import ShopPage from './pages/shopPage/shopPage'
 import Header from './components/header/header'
 import SignInUp from './pages/shopPage/sign-in-up/sign-in-up'
 import {auth, createUserProfileDocument} from '../src/firebase/firebase.utils'
+import {connect} from 'react-redux'
+import {setCurrentUser} from './redux/user/user.action'
 
 
 class App extends React.Component {
@@ -23,17 +25,17 @@ componentDidMount(){
     if(userAuth){
       const userRef=await createUserProfileDocument(userAuth) //userRef we got it from the config
       userRef.onSnapshot(snapshot=>{//same as onAuthStateChanged (used insted of get())
-        this.setState({//storing data in the state
+        this.props.setCurrentUser({//storing data in the state
           currentUser:{
             id:snapshot.id,//we use it to get the id, because it doesnt exist in the snapshot.data()
             ...snapshot.data()
           }
-        },()=>{ console.log(this.state)})
+        })
        
       });
     }
     else{
-      this.setState({currentUser:userAuth})//returns null alwayas
+      this.props.setCurrentUser(userAuth)//returns null alwayas
       console.log(this.state)
     }
     
@@ -50,11 +52,11 @@ componentWillUnmount(){
   return (
     <BrowserRouter>
     <div className="App"> 
-    <Header currentUser={this.state.currentUser}/>
+    <Header />
     <Switch>
       <Route exact path='/'component={HomePage}/>
       <Route exact path='/shop'component={ShopPage}/>
-      <Route exact path='/signin'component={SignInUp}/>
+      <Route exact path='/signin'render={()=>this.props.currentUser?(<Redirect to='/'/>):<SignInUp/>}/>
       </Switch>
     </div>
     </BrowserRouter>
@@ -62,4 +64,11 @@ componentWillUnmount(){
 }
 }
 
-export default App;
+const mapStateToProps=(state)=>({
+  currentUser:state.user.currentUser
+  })
+
+const mapDispatchToProps=(dispatch)=>({
+  setCurrentUser: (user)=>dispatch(setCurrentUser(user))
+})
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,22 @@
+import {userActionTypes} from './user.types'
+
+const INITIAL_STATE={
+    currentUser:null
+}
+
+//state is considered this.state
+//action is considered this.setState
+const userReducer =(state=INITIAL_STATE,action)=>{
+    switch(action.type){
+        case userActionTypes.SET_CURRENT_USER:
+            return{
+                ...state,
+                currentUser:action.payload
+            }
+
+        default:return state
+
+    }
+
+}
+export default userReducer
