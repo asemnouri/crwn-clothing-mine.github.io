2de81200d1b9768021f0c2c2c2ebde8995b5178f
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-06-27 20:48:18 +0300

    lec 127

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './checkout-item.scss'
+import {clearItemFromCart} from '../../redux/cart/cart.action'
 
-const CheckoutItem=({cartItem:{name,price,imageUrl,quantity}})=>(//we need to pass the whole item so that we can add items 
+const CheckoutItem=({cartItem,clearItemFromCart})=>{
+    const {name,price,imageUrl,quantity} =cartItem
+
+return(//we need to pass the whole item so that we can add items 
     <div className="checkout-item">
         <div className="image-container">
 {/* a container for the img so that i can control it without affecting the othe propeties in the call */}
@@ -11,7 +15,11 @@ const CheckoutItem=({cartItem:{name,price,imageUrl,quantity}})=>(//we need to pa
         <span className='name'>{name}</span>
         <span className='quantity'>{quantity}</span>
         <span className='price'>{price}</span>
-        <div className='remove-button'> &#10005;</div>
+        <div className='remove-button'onClick={()=>clearItemFromCart(cartItem)}> &#10005;</div>
     </div>
-)
-export default CheckoutItem
+)}
+
+const mapDispatchToProps=(dispatch)=>({
+    clearItemFromCart: (item)=>dispatch(clearItemFromCart(item))
+  })
+export default connect(null,mapDispatchToProps)(CheckoutItem)
