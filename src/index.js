a41dbcf3b1de3be0cab687523efa89edbce15013
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-06-27 22:57:39 +0300

    lec 131

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
-import store from './redux/store'
+import {persistGate} from 'redux-persist/integration/react'
+
+import {store, persistor} from './redux/store'
+
+
 
 ReactDOM.render(
     <React.StrictMode>
     <Provider store={store}>
+      <persistGate persistor={persistor}>
     <App />
+    </persistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
-import userReducer from './user/user.reducer'
-
+import {persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' //this is local storage(to get the session storage you type sessionStorage from another library)
 import {combineReducers} from 'redux'
-import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+import cartReducer from './cart/cart.reducer'
+import userReducer from './user/user.reducer'
+import directoryReducer from './directory/directory.reducer'
+
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['cart']//we need only the cart to be stored when refreshing because the use is saved in the firebase
+}
+
+const rootReducer=combineReducers({
     user:userReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    directory:directoryReducer
 })
+export default persistReducer(persistConfig,rootReducer)
